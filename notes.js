122e7ce01a88829015e879258291c402790e1175
Thomas Witherow <twitherow@bellsouth.net>
2021-05-28 15:05:21 -0500

    commit

---
+++ notes.js
@@ -0,0 +1,17 @@
+const notes = [
+    {
+        id: 1,
+        subject: "",
+        date: "",
+        feeling: "",
+        timeSpent: ""
+    },
+    {
+        id: 2,
+        subject: "",
+        date: "",
+        feeling: "",
+        timeSpent: ""
+    }
+]
+console.log(notes)
