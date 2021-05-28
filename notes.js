c38205d446ccc0d8e9c017219cb25f137d6d75ed
Thomas Witherow <twitherow@bellsouth.net>
2021-05-28 15:34:36 -0500

    added for/of loop

--- notes.js
+++ notes.js
@@ -1,17 +1,17 @@
 const notes = [
     {
         id: 1,
-        subject: "",
-        date: "",
-        feeling: "",
-        timeSpent: ""
+        subject: "HTML",
+        date: "05/25/21",
+        feeling: "confident",
+        timeSpent: "6 hours"
     },
     {
         id: 2,
-        subject: "",
-        date: "",
-        feeling: "",
-        timeSpent: ""
+        subject: "CSS",
+        date: "05/26/21",
+        feeling: "energetic",
+        timeSpent: "5 hours"
     }
 ]
 console.log(notes)      
@@ -27,3 +27,12 @@ const noteAboutToday = {
 notes.push(noteAboutToday)
 
 console.log(notes)
+
+for (const note of notes) {
+    console.log(`Note ${note.id}
+ ${note.date}
+ I learned ${note.subject}.
+ I spent ${note.timeSpent} working on it.
+ I feel ${note.feeling}
+ --------   `)
+}
